ca52ac1778f42a4005c1871632367521a64e5f7b
LAPTOP-CQOG6PAO\Playdata <null00007@naver.com>
2024-04-03 17:36:32 +0900

    init: "프로젝트 초기 설정"
    ArticleEntity 작성 중

--- itty-backend/src/main/java/org/iot/itty/article/aggregate/ArticleEntity.java
+++ itty-backend/src/main/java/org/iot/itty/article/aggregate/ArticleEntity.java
@@ -1,4 +1,25 @@
 package org.iot.itty.article.aggregate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "article_tb")
+@NoArgsConstructor
+@AllArgsConstructor
 public class ArticleEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "article_code_pk")
+	private Integer articlCodePk;
+
+	@Column(name = "article_title")
+	private String articleContent;
+
 }
