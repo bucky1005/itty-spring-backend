f668f7eff1395c66f5d02b4b5d17937ad28a691c
universe <hello.its.universe@gmail.com>
2024-04-04 12:45:31 +0900

    Feat: "자유게시판 전체조회 기능 구현"
    자유게시판 전체조회 기능 개발
    
    Resolves: #33

--- itty-backend/src/main/java/org/iot/itty/article/service/ArticleServiceImpl.java
+++ itty-backend/src/main/java/org/iot/itty/article/service/ArticleServiceImpl.java
@@ -1,4 +1,33 @@
 package org.iot.itty.article.service;
 
-public class ArticleServiceImpl {
+import java.util.List;
+
+import org.iot.itty.article.aggregate.ArticleEntity;
+import org.iot.itty.article.repository.ArticleRepository;
+import org.iot.itty.dto.ArticleDTO;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ArticleServiceImpl implements ArticleService{
+
+	private final ArticleRepository articleRepository;
+	private final ModelMapper mapper;
+
+	@Autowired
+	public ArticleServiceImpl(ArticleRepository articleRepository, ModelMapper mapper) {
+		this.articleRepository = articleRepository;
+		this.mapper = mapper;
+	}
+
+	@Override
+	public List<ArticleDTO> selectAllArticleFromFreeBoard(int articleCategory) {
+		List<ArticleEntity> articleEntityList = articleRepository.findAllByArticleCategory(articleCategory);
+
+		return articleEntityList
+			.stream()
+			.map(ArticleEntity -> mapper.map(ArticleEntity, ArticleDTO.class))
+			.toList();
+	}
 }
