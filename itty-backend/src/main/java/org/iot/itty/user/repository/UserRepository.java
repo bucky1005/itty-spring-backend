9f3a7ea2a2ae9ee5fcb34f1846517367eef86a45
shindaeyeong <eodud3196@gmail.com>
2024-04-04 11:59:20 +0900

    init: "User Repository"
    User Repository 초기 세팅

--- itty-backend/src/main/java/org/iot/itty/user/repository/UserRepository.java
+++ itty-backend/src/main/java/org/iot/itty/user/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package org.iot.itty.user.repository;
 
-public class UserRepository {
+import org.iot.itty.user.aggregate.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
 }
