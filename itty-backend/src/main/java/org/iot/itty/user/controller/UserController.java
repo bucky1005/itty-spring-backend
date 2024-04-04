f6b819cd1e7ae209400d4cdbeab91520b02e775b
shindaeyeong <eodud3196@gmail.com>
2024-04-04 11:47:49 +0900

    Feat: "Modify User"
    사용자 정보 수정 Controller 구현중

--- itty-backend/src/main/java/org/iot/itty/user/controller/UserController.java
+++ itty-backend/src/main/java/org/iot/itty/user/controller/UserController.java
@@ -1,8 +1,14 @@
 package org.iot.itty.user.controller;
 
+import org.iot.itty.dto.UserDTO;
 import org.iot.itty.user.service.UserService;
+import org.iot.itty.user.vo.ResponseModifyUser;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,4 +24,12 @@ public class UserController {
 		this.userService = userService;
 	}
 
+	@PostMapping("/user/modify")
+	public ResponseEntity<ResponseModifyUser> modifyUser(@RequestBody ResponseModifyUser modifyUserData){
+
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		UserDTO userDTO = modelMapper.map(modifyUserData, UserDTO.class);
+		UserDTO user = userService.modifyUser(userDTO);
+
+	}
 }
