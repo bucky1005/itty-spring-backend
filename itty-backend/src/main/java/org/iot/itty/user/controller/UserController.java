0a6845873ab5c04798164af12f35b2bf3d1bd4fd
shindaeyeong <eodud3196@gmail.com>
2024-04-04 11:23:35 +0900

    Init: "User Setting"
    User 초기 개발 세팅 완료.

--- itty-backend/src/main/java/org/iot/itty/user/controller/UserController.java
+++ itty-backend/src/main/java/org/iot/itty/user/controller/UserController.java
@@ -1,4 +1,21 @@
 package org.iot.itty.user.controller;
 
+import org.iot.itty.user.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
 public class UserController {
+	private final ModelMapper modelMapper;
+	private final UserService userService;
+
+	@Autowired
+	public UserController(ModelMapper modelMapper, UserService userService) {
+		this.modelMapper = modelMapper;
+		this.userService = userService;
+	}
+
 }
--- itty-backend/src/main/java/org/iot/itty/user/service/UserServiceImpl.java
+++ itty-backend/src/main/java/org/iot/itty/user/service/UserServiceImpl.java
@@ -1,4 +1,19 @@
 package org.iot.itty.user.service;
 
+import org.iot.itty.user.repository.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl {
+
+	private final ModelMapper modelMapper;
+	private final UserRepository userRepository;
+
+	@Autowired
+	public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository) {
+		this.modelMapper = modelMapper;
+		this.userRepository = userRepository;
+	}
 }
