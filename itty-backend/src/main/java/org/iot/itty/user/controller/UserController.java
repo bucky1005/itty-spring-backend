1c3d142f3d3470fe133712f241fba60329fd9e1b
shindaeyeong <eodud3196@gmail.com>
2024-04-04 12:33:34 +0900

    Feat: "UserController 반환문"
    유저 수정 Frontend Response 데이터 반환 구현

--- itty-backend/src/main/java/org/iot/itty/user/controller/UserController.java
+++ itty-backend/src/main/java/org/iot/itty/user/controller/UserController.java
@@ -2,10 +2,11 @@ package org.iot.itty.user.controller;
 
 import org.iot.itty.dto.UserDTO;
 import org.iot.itty.user.service.UserService;
-import org.iot.itty.user.vo.ResponseModifyUser;
+import org.iot.itty.user.vo.ResponseUserModify;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,11 +26,25 @@ public class UserController {
 	}
 
 	@PostMapping("/user/modify")
-	public ResponseEntity<ResponseModifyUser> modifyUser(@RequestBody ResponseModifyUser modifyUserData){
+	public ResponseEntity<ResponseUserModify> modifyUser(@RequestBody ResponseUserModify modifyUserData){
 
 		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 		UserDTO userDTO = modelMapper.map(modifyUserData, UserDTO.class);
 		UserDTO user = userService.modifyUser(userDTO);
 
+		ResponseUserModify responseUser = new ResponseUserModify();
+
+		if(user.getUserCodePk() == userDTO.getUserCodePk()){
+			if(user.getUserNickname().equals(userDTO.getUserNickname()) && user.getUserIntroduction().equals(userDTO.getUserIntroduction())) {
+				responseUser.setResultCode(200);
+				responseUser.setMessage("프로필 수정 완료 되었습니다.");
+			}
+		}
+		else{
+			responseUser.setResultCode(200);
+			responseUser.setMessage("비밀번호 확인 바랍니다.");
+		}
+
+		return ResponseEntity.status(HttpStatus.OK).body(responseUser);
 	}
 }
