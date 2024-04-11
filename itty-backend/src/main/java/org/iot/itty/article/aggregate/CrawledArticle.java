33c9da0ab3ddee04de4319f223fd35429b4b5260
universe <hello.its.universe@gmail.com>
2024-04-11 17:26:45 +0900

    Fix: "Branch init & ElasticSearch 제거"
    Branch init & ElasticSearch 제거

--- itty-backend/src/main/java/org/iot/itty/article/aggregate/CrawledArticle.java
+++ itty-backend/src/main/java/org/iot/itty/article/aggregate/CrawledArticle.java
@@ -1,16 +1,16 @@
-package org.iot.itty.article.aggregate;
-
-import java.util.Date;
-
-import lombok.Builder;
-import lombok.Data;
-
-@Data
-@Builder
-public class CrawledArticle {
-	private String image;
-	private String subject;
-	private String url;
-	private String content;
-	private String category;
-}
+// package org.iot.itty.article.aggregate;
+//
+// import java.util.Date;
+//
+// import lombok.Builder;
+// import lombok.Data;
+//
+// @Data
+// @Builder
+// public class CrawledArticle {
+// 	private String image;
+// 	private String subject;
+// 	private String url;
+// 	private String content;
+// 	private String category;
+// }
