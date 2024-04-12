d7094043f112400e73bbd41b0a51f1873aeb88fa
universe <hello.its.universe@gmail.com>
2024-04-12 09:01:47 +0900

    Feat: "트렌드 게시글 크롤링한 데이터 DB에서 삭제 기능 구현 완료"
    트렌드 게시글 크롤링한 데이터 DB에서 삭제 기능 구현 완료
    
    Resolves: #23

--- itty-backend/src/main/java/org/iot/itty/article/service/TrendArticleService.java
+++ itty-backend/src/main/java/org/iot/itty/article/service/TrendArticleService.java
@@ -10,4 +10,6 @@ public interface TrendArticleService {
 	List<TrendArticleDTO> selectAllTrendArticle();
 
 	List<TrendArticleDTO> addTrendArticle() throws IOException, ParseException;
+
+	String deleteTrendArticle(int trendArticleCodePk);
 }
